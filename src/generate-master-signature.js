f6476eb2780f529cd77499474cb3e6b7eb30ba49
Natheer_sharawi <natheernn3@gmail.com>
2026-01-01 23:18:29 +0300

    update generate signiture

---
+++ src/generate-master-signature.js
@@ -0,0 +1,27 @@
+const path = require('path');
+require('dotenv').config({
+  path: path.join(__dirname, '../.env.development')
+});
+const crypto = require('crypto');
+
+const SECRET_KEY = process.env.HMAC_SECRET_KEY;
+
+if (!SECRET_KEY) {
+  throw new Error('HMAC_SECRET_KEY is missing');
+}
+
+const timestamp = Date.now().toString();
+const data = `MASTER_KEY_BYPASS${timestamp}`;
+
+const signature = crypto
+  .createHmac('sha256', SECRET_KEY)
+  .update(data)
+  .digest('hex');
+
+console.log('🔑 UNIVERSAL MASTER SIGNATURE (Dev Only)');
+console.log('---------------------------------------------------');
+console.log('Use these headers for ANY request:');
+console.log(`X-Timestamp: ${timestamp}`);
+console.log(`X-Signature: ${signature}`);
+console.log('---------------------------------------------------');
+console.log('Expires in 5 minutes.');
